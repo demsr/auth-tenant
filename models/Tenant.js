8fa957ddad0c240768d24b9ca925bd0240591e3b
Dennis Sumser <46405520+demsr@users.noreply.github.com>
2021-08-30 22:16:40 +0200

    removed key load

--- models/Tenant.js
+++ models/Tenant.js
@@ -5,8 +5,6 @@ const jwt = require("jsonwebtoken");
 const fs = require("fs");
 const redis = require("../db/Redis");
 
-const privateKEY = fs.readFileSync("./private.key", "utf8");
-
 const Schema = new mongoose.Schema({
   name: String,
   apps: [String],
